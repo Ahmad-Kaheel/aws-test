8d6a2954c596180d6bf69879050cc4cf253b92f9
Ahmad-Kaheel <ahmadkahil596@gmail.com>
2022-04-16 05:11:45 +0300

    adding all files and codes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import Layout from './hocs/Layout';
+import Blog from './components/Blog';
+import BlogDetail from './components/BlogDetail';
+import Category from './components/Category';
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/" element={<Home />}/>
-        </Routes>
-      </div>
-    </Router>
+    <Router >
+    <Layout>
+      <Routes >
+        <Route path = '/' element = {<Blog/>} />
+        <Route path = '/category/:category' element = {<Category/>} />
+        <Route path = '/blog/:id' element = {<BlogDetail/>} />
+      </Routes>
+    </Layout>
+  </Router>
   );
 }
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
-
-// export default App;
