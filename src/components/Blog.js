d82861e8ac156cd2f2c5741b034aba374fa4fc87
Ahmad-Kaheel <ahmadkahil596@gmail.com>
2022-04-19 06:39:02 +0300

    Adding title

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -66,6 +66,7 @@ const Blog = () => {
                     <Link className="nav-link active " to='/category/business'>Business</Link>
                 </nav>
             </div>
+            title = {featuredBlog.title}
             <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
                 <div className="col-md-6 px-0">
                     <h1 className="display-4 font-italic">{featuredBlog.title}</h1>
